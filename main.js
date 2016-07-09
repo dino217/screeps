cf195d2ed4e492c1c2771a9380567a00520b1fdc
dino217 <brob217@gmail.com>
2016-07-08 18:18:24 -0700

    Move harvester code to role.harvester file.

--- main.js
+++ main.js
@@ -1,29 +1,12 @@
+"use strict";
+var roleHarvester = require('role.harvester');
+
 module.exports.loop = function () {
   for(let name in Game.creeps) {
     var creep = Game.creeps[name];
 
-    if (creep.memory.working && creep.carry.energy == 0){
-      creep.memory.working = false;
-    }
-
-    if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.working = true;
-    }
-
-    // If the creep has energy it should do work
-    if (creep.memory.working) {
-      var spawn = Game.spawns.Spawn1;
-      if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(spawn);
-      }
-    }
-
-    if (!creep.memory.working) {
-      var source = creep.pos.findClosestByPath(FIND_SOURCES);
-
-      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(source);
-      }
+    if (creep.memory.role === 'harvester'){
+      roleHarvester.run(creep);
     }
   }
 }
---
+++ role.harvester.js
@@ -0,0 +1,28 @@
+module.exports = {
+  run: function(creep) {
+    // If the harvester is working, but runs out of energy, it should switch to not working.
+    if (creep.memory.working && creep.carry.energy == 0){
+      creep.memory.working = false;
+    }
+    // If the harvester has filled up on energy it should switch to working.
+    if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.working = true;
+    }
+
+    // If the creep has energy it should do work
+    if (creep.memory.working) {
+      var spawn = Game.spawns.Spawn1;
+      if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(spawn);
+      }
+    }
+    // If the harvester is not working, it should get energy so it can work.
+    if (!creep.memory.working) {
+      var source = creep.pos.findClosestByPath(FIND_SOURCES);
+
+      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+      }
+    }
+  }
+}
