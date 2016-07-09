880f04e6b9e2d1902be8540aecc2307e045eb48e
dino217 <brob217@gmail.com>
2016-07-09 07:41:30 -0700

    Fixes crash in harvester depositing.

--- role.harvester.js
+++ role.harvester.js
@@ -12,7 +12,7 @@ module.exports = {
     // If the creep has energy it should do work
     if (creep.memory.working) {
       var spawn = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-        filter: (s.structureType == STRUCTURE_SPAWN
+        filter: (s) => (s.structureType == STRUCTURE_SPAWN
               || s.structureType == STRUCTURE_EXTENSION)
               && s.energy < s.energyCapacity
       })
